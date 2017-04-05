d2616fc3145e271c0555f236793cf4123074d543
Mateo TibaPalacios <nestormateo@gmail.com>
2017-04-04 21:02:45 -0500

    Moved: Animation from app-root to ng-wizard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,8 @@
-import { Component, HostBinding, Input } from '@angular/core';
-import { slideInDownAnimation } from './animations';
-import { WizardAction } from './states';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  animations: [ slideInDownAnimation ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  @HostBinding('@routeAnimation') routeAnimation = true;
-  @HostBinding('style.padding') padding = '50px 0';
-  @HostBinding('style.display')   display = 'block';
-  // @HostBinding('style.position')  position = 'absolute';
-  @HostBinding('style.backgroundColor') backgroundColor = '#1976D2';
-  @HostBinding('style.color') color = 'white';
-
-  @Input() param;
-
-  actions: WizardAction[] = [];
-  haveFooter: boolean = true;
-}
+export class AppComponent {}
