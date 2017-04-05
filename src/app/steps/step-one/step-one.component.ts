db9dc29d14b523564232707362f4bf197515ca04
Mateo TibaPalacios <nestormateo@gmail.com>
2017-04-04 21:20:45 -0500

    Moved: animations to wizard step

--- src/app/steps/step-one/step-one.component.ts
+++ src/app/steps/step-one/step-one.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, Input  } from '@angular/core';
+import { slideInDownAnimation } from '../../animations';
+import { WizardAction } from '../../states';
 
 @Component({
   selector: 'step-one',
   templateUrl: './step-one.component.html',
-  styleUrls: ['./step-one.component.css']
+  styleUrls: ['./step-one.component.css'],
+  animations: [ slideInDownAnimation ]
 })
 export class StepOneComponent {
+
+  @HostBinding('@routeAnimation') routeAnimation = true;
+  @HostBinding('style.display')   display = 'block';
+  @HostBinding('style.padding')   padding = '20px';
+  // @HostBinding('style.position')  position = 'absolute';
+  @HostBinding('style.backgroundColor') backgroundColor = '#563d7c';
+  @HostBinding('style.color') color = 'white';
+
+  @Input() param;
+
+  actions: WizardAction[] = [];
+  haveFooter: boolean = true;
 
   constructor() { }
 
--- src/app/wizard/wizard.component.ts
+++ src/app/wizard/wizard.component.ts
@@ -1,26 +1,14 @@
-import { Component, AfterContentInit, OnDestroy, HostBinding, Input } from '@angular/core';
+import { Component, AfterContentInit, OnDestroy} from '@angular/core';
 import { Store } from '@ngrx/store';
-import { slideInDownAnimation } from '../animations';
 
-import { WizardState, WizardAction } from '../states';
+import { WizardState } from '../states';
 
 @Component({
   selector: 'ng-wizard',
   templateUrl: './wizard.component.html',
-  styleUrls: ['./wizard.component.css'],
-  animations: [ slideInDownAnimation ]
+  styleUrls: ['./wizard.component.css']
 })
 export class WizardComponent implements AfterContentInit, OnDestroy {
-  @HostBinding('@routeAnimation') routeAnimation = true;
-  @HostBinding('style.display')   display = 'block';
-  // @HostBinding('style.position')  position = 'absolute';
-  // @HostBinding('style.backgroundColor') backgroundColor = '#1976D2';
-  @HostBinding('style.color') color = 'white';
-
-  @Input() param;
-
-  actions: WizardAction[] = [];
-  haveFooter: boolean = true;
 
   private subscription;
   state: WizardState;
